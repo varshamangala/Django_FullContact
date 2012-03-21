2a8838e91524dfdd6ba2a9bbbf01008a7122f6a6
Varsha Mangala <varshamangala@gmail.com>
2012-03-21 17:07:57 +0530

    first commit

---
+++ models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class UserProfile(models.Model):
+	emailid=models.EmailField(primary_key=True)
+	userdata=models.TextField()
+	modifyts=models.DateField(auto_now=True)
+
+	def _unicode_(self):
+		return self.emailid
+
+class FullContactLogger(models.Model):
+	emailid=models.EmailField()
+	statuscode=models.IntegerField(db_index=True)
+	createts=models.DateTimeField(auto_now_add=True,db_index=True)
+	
+	def _unicode_(self):
+		return self.emailid, self.statuscode, self.createts
